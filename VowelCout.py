a541154b58a372ac1deeb354cceec92c42d08e8e
stasonucin <discordantstas@icloud.com>
2022-11-16 20:50:42 +0300

    #Vowel Cout

---
+++ VowelCout.py
@@ -0,0 +1,15 @@
+def get_count(sentence):
+    simple_set = ("a", "e", "i", "o", "u")
+    simple_arr = []
+    for i in range(len(list(sentence))):
+        if sentence[i] in simple_set:
+            simple_arr.append(sentence[i])
+    return len(simple_arr)
+
+
+#Best answer from Codewars
+#javafreak, ChingChangChong, hdang101, ronanodufaigh, rbordiya, alpharom, lukegb21,
+# TRUUUHaZe, mwdbjbc, hectorperez_net (+ 47)
+
+#def getCount(inputStr):
+#    return sum(1 for let in inputStr if let in "aeiouAEIOU")
