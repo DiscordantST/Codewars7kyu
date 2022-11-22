90db46052d8b0f1b5b8529de80f68bc97847f157
DiscordantST <discordantstas@gmail.com>
2022-11-23 00:45:33 +0300

    #Descending order

--- DescendingOrder.py
+++ DescendingOrder.py
@@ -1,6 +1,12 @@
 def descending_order(num):
-    return sorted([(str(num))], key=len, reverse=True)
+    a = str(num)
+    c = []
+    b = []
+    for i in range(len(a)):
+        c.append(a[i])
+    c.sort(reverse=True)
+    for i in range(len(c)):
+        b.append(c[i])
+    b = "".join(b)
+    return int(b)
 
-
-
-print(descending_order(123124))
