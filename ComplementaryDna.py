9ceab1e9818dd67ec77f321c5a577983b2778837
DiscordantST <discordantstas@gmail.com>
2022-11-24 21:39:22 +0300

    #solution ComplementaryDna.py

--- ComplementaryDna.py
+++ ComplementaryDna.py
@@ -15,3 +15,4 @@ def DNA_strand(dna):
 # Azuaron, yannche, MolloOXx, D0425434, rxsilva, dprez808, pklecha, Holly Olly Oxen Free, AcostaDavid, DrMoriarty74 (+ 48)
 # def DNA_strand(dna):
 #   return "".join([{'A':'T', 'T':'A', 'C':'G', 'G':'C'}[l] for l in dna])
+
