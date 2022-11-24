f20c258184eabce7256faee68c086e56ca0c5625
DiscordantST <discordantstas@gmail.com>
2022-11-24 13:11:24 +0300

    #solution Get middle Character

--- GetTheMiddleCharacter.py
+++ GetTheMiddleCharacter.py
@@ -1,2 +1,7 @@
 def get_middle(s):
-    pass
+    if len(s) % 2 == 0:
+        a = int(len(s)) // 2
+        return s[a - 1:a + 1]
+    if len(s) % 2 != 0:
+        a = int(len(s)) // 2
+        return s[a]
