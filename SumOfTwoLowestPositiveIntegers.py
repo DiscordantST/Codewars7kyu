f49eb71c2afcfe99a6b28ac733f0acdbdbecd096
DiscordantST <discordantstas@gmail.com>
2022-11-25 16:35:13 +0300

    #Add solution SumOfTwoLowestPositiveIntegers.py

--- SumOfTwoLowestPositiveIntegers.py
+++ SumOfTwoLowestPositiveIntegers.py
@@ -1,2 +1,10 @@
 def sum_two_smallest_numbers(numbers):
-    pass
+    first_digit = min(numbers)
+    numbers.remove(min(numbers))
+    two_digit = min(numbers)
+    return first_digit + two_digit
+
+
+#  another answer:
+# def sum_two_smallest_numbers(numbers):
+#     return sum(sorted(numbers)[:2])
