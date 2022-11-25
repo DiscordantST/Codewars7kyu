67a1c8a92fea46bf8d8421047ea6ff68f7c8a582
DiscordantST <discordantstas@gmail.com>
2022-11-25 23:00:06 +0300

    #Add solution BeginnerSeries.py

--- BeginnerSeries.py
+++ BeginnerSeries.py
@@ -1,3 +1,9 @@
-def get_sum(a,b):
-    pass
-
+def get_sum(a, b):
+    sum_digit = 0
+    if a == b:
+        print(a)
+    if b < a:
+        a, b = b, a
+    for i in range(a, b + 1):
+        sum_digit += i
+    return sum_digit
